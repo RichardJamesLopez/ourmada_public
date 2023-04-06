5417a62385c18086854b7fb9e1a7bec2118ad7a9
Rich CL <richcl@richs-air.lan>
2023-04-06 11:14:10 -0400

    Merge changes from remote repository

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Pronounced OUR mada</title>
+        <title>we ship DAO tools</title>
         <meta name="description" content="quick Splash page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>

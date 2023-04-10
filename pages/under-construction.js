a988ddab0ddf894b104ac5bfff6902fcf524c476
Rich CL <richcl@richs-air.lan>
2023-04-10 10:49:08 -0400

    Merge changes from remote repository

--- pages/under-construction.js
+++ pages/under-construction.js
@@ -1,4 +1,6 @@
 import styles from '@/styles/under-construction.module.css';
+import Image from 'next/image';
+import constructionImage from '@/public/underconstruction.png';
 
 export default function UnderConstruction() {
   return (
@@ -6,6 +8,7 @@ export default function UnderConstruction() {
       <main className={styles['main-construction']} style={{ fontSize: '20px', color: '#4285f4'}}>
         <div className={styles.content}>
         <h1 className={styles.code}>Under Construction</h1>
+        <Image src={constructionImage} width="400" height="400" />
         <p>We are currently buidling. &nbsp;
         </p>
         <p>Talk to us if you want to learn more. </p>
